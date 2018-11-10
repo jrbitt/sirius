55a91518b1a0d12ca83520a6b3c75e78a2464256
João Ricardo Bittencourt <JOAORB@unisinos.br>
2018-11-10 18:47:47 -0200

    Virtual Keyboard

--- addons/virtual_keyboard/scripts/Key.gd
+++ addons/virtual_keyboard/scripts/Key.gd
@@ -1,16 +1,43 @@
+tool
 extends Control
 
-export(String) var tecla = "A"
-export(Texture) var botao_normal setget _set_textura
+export(String) var key = "A" setget _set_key
+export(Theme) var key_theme setget _set_theme
+export(Texture) var normal_key setget _set_texture
+export(bool) var special = false setget _set_special
 
-signal botao_pressionado(t)
+signal key_pressed(t)
 
-func _set_textura(t):
-	botao_normal = t
-	$botao.texture_normal = botao_normal
+func _set_special(s):
+	special = s
+	if has_node("label"):
+		$label.visible = not special
+	
+func _set_key(k):
+	key = k
+	if has_node("label"):
+		$label.text = key
+	update()
+	
+func _set_theme(t):
+	theme = t
+	if has_node("label"):
+		$label.theme = theme
+	update()
+	
+func _set_texture(t):
+	normal_key = t
+	if has_node("image"):
+		$image.texture_normal = normal_key
+	update()
 	
 func _ready():
-	$botao.texture_normal = botao_normal
+	if not Engine.editor_hint:
+		$image.texture_normal = normal_key
+		_set_special(special)
+		if not special:
+			$label.text = key
+			$label.theme = theme
 
-func _on_botao_button_up():
-	emit_signal("botao_pressionado",tecla)
+func _on_image_button_up():
+	emit_signal("key_pressed",key)
--- addons/virtual_keyboard/scripts/Keyboard.gd
+++ addons/virtual_keyboard/scripts/Keyboard.gd
@@ -1,14 +1,54 @@
+tool
 extends Control
 
+export(Texture) var normal_simple_key = null setget _set_texture
+export(Theme) var key_theme setget _set_theme
 var string = ""
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+onready var lines = $lines
 
-func concatenar(t):
-	string = string + t
+signal key_pressed(k)
+signal enter_pressed(s)
+
+func _set_texture(t):
+	normal_simple_key = t	
+	if has_node("lines"):
+		lines = $lines
+		for c in lines.get_children():
+			for b in c.get_children():
+				if not b.special: 
+					b.normal_key = normal_simple_key
+	update()
+
+func _set_theme(t):
+	key_theme = t
+	if has_node("lines"):
+		lines = $lines
+		for c in lines.get_children():
+			for b in c.get_children():
+				b.key_theme = key_theme
+	update()
 	
-func _on_b1_botao_pressionado(t):
-	concatenar(t)
+func _ready():
+	if not Engine.editor_hint:
+		for c in lines.get_children():
+			for b in c.get_children():
+				if not b.special:
+					b.normal_key = normal_simple_key
+				b.key_theme = key_theme
+				b.connect("key_pressed",self,"on_key_pressed")
+
+func concat(t):
+	if t == "space":
+		string = string + " "
+	else:
+		string = string + t
+	
+func on_key_pressed(k):
+	if k == "enter":
+		emit_signal("enter_pressed",string)
+		string = ""
+	else:
+		emit_signal("key_pressed",k)
+		concat(k)
+	print(k)
